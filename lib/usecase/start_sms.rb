2433ab058bc71066d5e1acbd579322c462f80c0b
Parth Shah <parthshahva@gmail.com>
2014-04-07 14:58:52 -0500

    please work on first try

--- lib/usecase/start_sms.rb
+++ lib/usecase/start_sms.rb
@@ -1,5 +1,18 @@
 class StartSMS < UseCase
   def run(inputs)
+     user = User.first(phone_number: inputs[:phone_number])
+     return failure(:user_does_not_exist) if user.nil?
 
+     question_set = Questionset.get(inputs[:question_set_name])
+     return failure(:question_set_not_found) if user.nil?
+
+     questions = Question.all(:question_id => question_set.id)
+     number = questions.count - 1
+     current_question_id = rand(0..number)
+     current_question = Question.get(current_question_id)
+     user.last_question_id = current_question_id
+     user.save
+     message = current_question.text
+     success :mesage => message
   end
 end
