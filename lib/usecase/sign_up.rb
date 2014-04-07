48106d170fb75de13193f71548f776c2d4dcd191
Parth Shah <parthshahva@gmail.com>
2014-04-07 11:46:28 -0500

    updated usecase

--- lib/usecase/sign_up.rb
+++ lib/usecase/sign_up.rb
@@ -1,12 +1,12 @@
 class SignUp < UseCase
   def run(inputs)
     user = User.first(inputs[:username])
-    return failure(:username_taken) if user != nil
+    return failure(:username_taken) if user.username == inputs[:username]
     return failure(:password_too_short) if inputs[:password].length < 4
     phone_number = inputs[:phone_number].delete("^0-9")
 
     return failure(:not_a_valid_number) if phone_number.length < 10
-    return failure(:phone_number_already_in_use) if User.first(inputs[:phone_number]) != nil
+    return failure(:phone_number_already_in_use) if User.first(inputs[:phone_number]).phone_number == inputs[:phone_number]
 
     user = User.create(username: inputs[:username], password: inputs[:password], phone_number: inputs[:phone_number])
     success :user => user
