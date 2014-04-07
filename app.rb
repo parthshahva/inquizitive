1d47eff965439b581b0d295b3bcb13655daf1c35
Moiz Jangda <macdaddy@moizs-mbp.att.net>
2014-04-07 11:03:05 -0500

    added home.erb and sign-up.erb

--- app.rb
+++ app.rb
@@ -2,10 +2,20 @@ require 'sinatra'
 enable :sessions
 require_relative 'lib/inquizitive.rb'
 
+set :bind, "0.0.0.0"
+
 configure :production do
   DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
 
 get '/' do
-"Test"
+  erb :index
+end
+
+post '/sign-in' do
+
+end
+
+get '/sign-up' do
+  erb :"sign-up"
 end
