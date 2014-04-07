9ec54f47e4b4dd083e8c20998486ca8ae004e4d9
Parth Shah <parthshahva@gmail.com>
2014-04-07 17:21:46 -0500

    question passing

--- lib/usecase/start_sms.rb
+++ lib/usecase/start_sms.rb
@@ -9,9 +9,9 @@ class StartSMS < UseCase
      questions = Question.all(:questionset_id => question_set.id)
      return failure(:no_questions_in_set) if questions.size == 0
 
-     number = questions.size - 1
-     current_question_id = rand(0..number)
-     current_question = Question.get(1)
+     number = questions.size
+     current_question_id = rand(1..number)
+     current_question = Question.get(current_question_id)
      user.last_question_id = current_question_id
      user.save
      message = current_question.text
