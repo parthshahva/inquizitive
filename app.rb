8c7b6d15137aece5891371a9052be7a94e7734ea
Parth Shah <parthshahva@gmail.com>
2014-04-07 10:02:11 -0500

    work now please

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 enable :sessions
 require_relative 'lib/inquizitive.rb'
 
-configure do, :group => :production
+configure do :group => :production
   DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
 
