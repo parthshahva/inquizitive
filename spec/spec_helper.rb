c8f72b12e23e6db3afbbd0eff2f61c0a10623f88
vagrant <vagrant@student-dev-box>
2014-04-07 17:10:34 +0000

    set up RSpec config in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,18 @@
 require_relative '../lib/inquizitive.rb'
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+  DataMapper::setup(:default, "sqlite:///inquizitive.db")
+
+  DataMapper.finalize
+  User.auto_migrate!
+  Questionset.auto_migrate!
+  Question.auto_migrate!
+  Response.auto_migrate!
+  Session.auto_migrate!
+
+end
+
+def app
+  Sinatra::Application
+end
