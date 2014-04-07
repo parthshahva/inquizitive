c01cd0f4d625feec26957fd335a492b45aa1c9b2
Parth Shah <parthshahva@gmail.com>
2014-04-07 10:12:49 -0500

    removed MBP module

--- lib/db.rb
+++ lib/db.rb
@@ -2,38 +2,7 @@ require 'dm-core'
 require 'dm-migrations'
 require 'dm-constraints'
 
-module MBP
 
-class Question
-  include DataMapper::Resource
-  property :id, Serial
-  property :text, String
-  property :answer, String
-  belongs_to :questionset
-end
-DataMapper.finalize
-class Questionset
-  include DataMapper::Resource
-  property :id, Serial
-  property :name, String
-  belongs_to :user
-  has n, :question
-end
-DataMapper.finalize
-class Response
-  include DataMapper::Resource
-  property :id, Serial
-  property :correct, Boolean
-  belongs_to :question
-  belongs_to :user
-end
-DataMapper.finalize
-class Session
-  include DataMapper::Resource
-  property :id, Serial
-  belongs_to :user
-end
-DataMapper.finalize
 class User
   include DataMapper::Resource
   property :id, Serial
@@ -45,4 +14,37 @@ class User
   has n, :session
 end
 DataMapper.finalize
+class Questionset
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String
+  belongs_to :user
+  has n, :question
 end
+DataMapper.finalize
+
+class Question
+  include DataMapper::Resource
+  property :id, Serial
+  property :text, String
+  property :answer, String
+  belongs_to :questionset
+end
+DataMapper.finalize
+
+class Response
+  include DataMapper::Resource
+  property :id, Serial
+  property :correct, Boolean
+  belongs_to :question
+  belongs_to :user
+end
+DataMapper.finalize
+
+class Session
+  include DataMapper::Resource
+  property :id, Serial
+  belongs_to :user
+end
+DataMapper.finalize
+
