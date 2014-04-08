d4b11fa350adde6b4dc91e460896222571d63048
Brian Provost <brianmprovost@gmail.com>
2014-04-08 15:28:42 -0500

    remove spaces from phone number

--- lib/usecase/sign_up.rb
+++ lib/usecase/sign_up.rb
@@ -3,7 +3,7 @@ class SignUp < UseCase
     user = User.first(inputs[:username])
     return failure(:username_taken) if user.username == inputs[:username]
     return failure(:password_too_short) if inputs[:password].length < 4
-    phone_number = inputs[:phone_number].delete("^0-9")
+    phone_number = inputs[:phone_number].delete("^0-9").gsub(" ", "")
 
     return failure(:not_a_valid_number) if phone_number.length < 10
     return failure(:phone_number_already_in_use) if User.first(inputs[:phone_number]).phone_number == inputs[:phone_number]
